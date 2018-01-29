3e60b4e13f62fea8e646c68c4a55a9bf0882945f
Carlo <carlo@RUSHs-MacBook-Pro.local>
2018-01-29 15:30:56 -0700

    mock data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import numeral from 'numeral';
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`);
@@ -21,6 +21,18 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
 
 
